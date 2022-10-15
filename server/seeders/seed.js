e97b78ea505671facff76c42b3cbaf7e94057d09
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-10-15 13:12:37 -0700

    updated seed logic

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,8 @@
 const db = require('../config/connection');
-const { Profile, Company } = require('../models');
+const { Profile, Company, Employee } = require('../models');
 const profileSeeds = require('./profileSeeds.json');
 const companySeeds = require('./companySeeds.json');
+const employeeSeeds = require('./employeeSeeds.json');
 
 db.once('open', async () => {
   try {
@@ -11,11 +12,20 @@ db.once('open', async () => {
     await Company.deleteMany({});
     await Company.create(companySeeds);
 
+    await Employee.deleteMany({});
+    await Employee.create(employeeSeeds);
+
     const owner = await Profile.findOne({name: 'Brian Kernighan'})
     const company = await Company.findOne({name: 'Brian Kernighans Sole care'})
+    const employee = await Employee.findOne({name: 'Brian Jr. Kernighans'})
     owner.company.push(company._id);
-    console.log(owner);
-    console.log(company);
+    company.employees.push(employee._id);
+
+    const updatedOwner = await owner.save()
+    const updatedCompany = await company.save()
+    console.log(updatedOwner)
+    console.log(updatedCompany)
+    console.log(employee);
 
     console.log('all done!');
     process.exit(0);
