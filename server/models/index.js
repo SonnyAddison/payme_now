551351c3c3b647a7a1805250f3927c553a6dfb22
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-10-16 22:48:58 -0700

    removed typeDef of company, updated to just using the Profile as the company.  Employee was added under profile to be an array instaed of company

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
-const Company = require("./Company");
+
 const Employee = require("./Employee");
 const Profile = require("./Profile");
 
-module.exports = { Company, Employee, Profile };
+module.exports = { Employee, Profile };
